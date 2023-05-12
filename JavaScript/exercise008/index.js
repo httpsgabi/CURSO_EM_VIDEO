f7c9f8e8f6df4aa0ff29575a96d66da5d3fa76fc
Gabrielle <gabriellemaia10@hotmail.com.br>
2023-05-11 21:36:16 -0300

    .

---
+++ JavaScript/exercise008/index.js
@@ -0,0 +1,7 @@
+var velocidade = 12; 
+
+    if (velocidade >=  60){
+        console.log('Você está acima da velocidade permitida portanto está multado. Dirija com consciência!');
+    } 
+
+console.log('Dirija sempre com cinto de segurança!');
